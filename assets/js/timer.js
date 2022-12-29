542963f301c70d54b8a696b09bd734487f120b55
iwasaki501 <a.iwasaki0128@gmail.com>
2022-12-29 12:14:45 +0900

    add web timer and history function to timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,8 +1,47 @@
 let work_timer = document.work_timer;
 let rest_timer = document.rest_timer;
 let total_working_timer = document.total_working_timer;
+
+work_timer.worker = new Worker("/assets/js/worker.js");
+work_timer.worker.onmessage = function (e) {
+  cntDown(work_timer);
+};
+rest_timer.worker = new Worker("/assets/js/worker.js");
+rest_timer.worker.onmessage = function (e) {
+  cntDown(rest_timer);
+};
+total_working_timer.worker = new Worker("/assets/js/worker.js");
+total_working_timer.worker.onmessage = function (e) {
+  cntUp(total_working_timer);
+};
+
 let button = document.getElementById("switch_mode_button");
+let task_input = document.getElementById("task_input");
+let table = document.getElementById("history_table");
 let timer_status = "not_started";
+
+function addRow(table, date_str, task_str) {
+  var tr = table.insertRow(-1);
+  var th = document.createElement("th");
+  th.innerHTML = date_str;
+  tr.appendChild(th);
+  var cell = tr.insertCell(1);
+  cell.innerHTML = task_str;
+}
+
+function createDateStr() {
+  let zero_padding = function (i) {
+    return i.toString().padStart(2, "0");
+  };
+  let d = new Date();
+  let date_str = `${d.getFullYear()}-${zero_padding(
+    d.getMonth() + 1
+  )}-${zero_padding(d.getDate())}`;
+  let time_str = `${zero_padding(d.getHours())}:${zero_padding(
+    d.getMinutes()
+  )}`;
+  return `${date_str} ${time_str}`;
+}
 
 function calcTime(timer) {
   let hour = parseInt(timer.elements[0].value);
@@ -45,42 +84,39 @@ function cntUp(timer) {
   processTime(timer, time + 1);
 }
 
-function cntStart(timer) {
-  timer.interval = setInterval(cntDown, 1000, timer);
-}
-
-function cntUpStart(timer) {
-  timer.interval = setInterval(cntUp, 1000, timer);
-}
-
-function cntStop(timer) {
-  clearInterval(timer.interval);
-}
-
 function take_lunch() {
   timer_status = "rest";
   button.value = "Back to work";
   setTime(rest_timer, calcTime(rest_timer) + 1800);
-  cntStop(work_timer);
-  cntStop(total_working_timer);
-  cntStart(rest_timer);
+  work_timer.worker.postMessage(0);
+  total_working_timer.worker.postMessage(0);
+  rest_timer.worker.postMessage(1);
   document.getElementById("take_lunch_button").style.display = "none";
+  addRow(table, createDateStr(), `Take a break`);
 }
 
 function switch_mode() {
+  let get_task_str = function () {
+    let task_value = task_input.value;
+    if (task_value === "") return "working";
+    return task_value;
+  };
   if (timer_status == "not_started") {
     // not started -> work
     timer_status = "work";
     button.value = "Take a rest";
-    cntStart(work_timer);
-    cntUpStart(total_working_timer);
+    work_timer.worker.postMessage(1);
+    total_working_timer.worker.postMessage(1);
+    document.getElementById("history").innerHTML = "<h2>History</h2>";
+    addRow(table, createDateStr(), `Start ${get_task_str()}`);
   } else if (timer_status == "work") {
     // work -> rest
     timer_status = "rest";
     button.value = "Back to work";
-    cntStop(work_timer);
-    cntStop(total_working_timer);
-    cntStart(rest_timer);
+    work_timer.worker.postMessage(0);
+    total_working_timer.worker.postMessage(0);
+    rest_timer.worker.postMessage(1);
+    addRow(table, createDateStr(), `Take a rest`);
   } else {
     // rest -> work
     timer_status = "work";
@@ -90,8 +126,9 @@ function switch_mode() {
       setTime(work_timer, calcTime(work_timer) - calcTime(rest_timer));
       setTime(rest_timer, 0);
     }
-    cntStop(rest_timer);
-    cntStart(work_timer);
-    cntUpStart(total_working_timer);
+    rest_timer.worker.postMessage(0);
+    work_timer.worker.postMessage(1);
+    total_working_timer.worker.postMessage(1);
+    addRow(table, createDateStr(), `Start ${get_task_str()}`);
   }
 }
--- timer.md
+++ timer.md
@@ -8,30 +8,38 @@ tags: [JavaScript, Product]
 
 <audio id="audio"></audio>
 
+<div id="task">
+<input id="task_input" type="text" placeholder="Type your task">
+</div>
+
 <div id="buttons">
-<input id="switch_mode_button" type="button" value="Start Working" onclick="switch_mode()">
+<input id="switch_mode_button" type="button" value="Start working" onclick="switch_mode()">
 <input id="take_lunch_button" type="button" value="Take a 30 min break" onclick="take_lunch()">
 </div>
 
 <div id="forms">
-<h2>Work Timer</h2>
+<h2>Work Block Timer</h2>
 <form name="work_timer">
 <input type="number" value=0 readonly>:<input type="number" value=25 readonly>:<input type="number" value=0 readonly>
-<!-- <input id="working_hour" value=0>hour<input id="working_min" value=25>min<input id="working_sec" value=0>sec -->
 </form>
 
 <h2>Rest Timer</h2>
 <form name="rest_timer">
 <input type="number" value=0 readonly>:<input type="number" value=0 readonly>:<input type="number" value=0 readonly>
-<!-- <input id="rest_hour" value=0>hour<input id="rest_min" value=0>min<input id="rest_sec" value=0>sec -->
 </form>
 
 <h2>Total Working Time</h2>
 <form name="total_working_timer">
 <input type="number" value=0 readonly>:<input type="number" value=0 readonly>:<input type="number" value=0 readonly>
-<!-- <input id="rest_hour" value=0>hour<input id="rest_min" value=0>min<input id="rest_sec" value=0>sec -->
 </form>
 </div>
+
+<div id="history">
+</div>
+<div class="table-wrapper"><table id="history_table">
+  <tbody>
+  </tbody>
+</table></div>
 
 <script type="text/javascript" src="./assets/js/timer.js"></script>
 
@@ -45,17 +53,33 @@ form {
   color: gray;
 }
 
+input[type="text"] {
+  width: 75%;
+  font-size: 1.2vmax;
+  background: none;
+  text-align: center;
+  border: 1px solid #aaa;
+  margin-bottom: 40px;
+  border-radius:6px;
+}
+
+input[type="text"]:focus {
+  outline: none;
+}
+
+input[type="text"]:focus::placeholder{
+  opacity: 0;
+}
+
 input[type="number"] {
-  /* width: 200px; */
   width: 28%;
   background: none;
-  /* font-size: 100px; */
   font-size: 4vmax;
   text-align: center;
 }
 
 input[type="button"] {
-  /* width: 45%; */
+  font-size: 1.2vmax;
   width: 300px;
   max-width: 90%;
   margin-right: 20px;
@@ -63,12 +87,33 @@ input[type="button"] {
   margin-bottom: 20px;
 }
 
+table th {
+  padding-left: 20px;
+  width:30%;
+}
+
+#task {
+  text-align: center;
+}
+
 #buttons {
   text-align: center;
 }
 
 #forms {
+  margin-top: 20px;
   text-align: center;
+}
+
+#history {
+  text-align: center;
+}
+
+#history_table {
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 75%;
 }
 
 </style>
